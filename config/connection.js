eac3fad3bb34b27010f319a37936430a7960517c
jenenneihart <neihart.jenen@gmail.com>
2020-05-09 15:19:57 -0700

    connecting to sql

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,19 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: ""
+    password: "password",
+    database: "burgers_db"
   });
   
+  // Attempting to Connect
+  connection.connect(function(err){
+     if err { console.error("Can not connect")};
+     return;
+   })
+console.log("You are connected as" + connection.threadId)
+  });
+  
+  
+  
+  //export coonnection for ORM 
+  module.exports= connection
